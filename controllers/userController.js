11c4adc873af17266fd29ceeffbe1158456d6780
sithija-ws <sithija.ws@gmail.com>
2026-05-15 22:44:05 +0530

    feat: Implement user registration and add CORS support

---
+++ controllers/userController.js
@@ -0,0 +1,41 @@
+import bcrypt from "bcrypt"
+
+export const userRegister = async (req,res)=>{
+    try {
+        let {email, username, password,role} = req.body;
+
+        if(!email || !username || !password || !role){
+            return res.status(400).json({
+                message: "Please fill all required fields!"
+            });
+        }
+
+        let existingUser = await User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({
+                message: "User already exists!"
+            });
+        }
+
+        let hashedPassword = await bcrypt.hash(password, 10);
+
+        let newUser = await User.create({
+            email,
+            username,
+            password: hashedPassword,
+            role
+        })
+
+        return res.status(201).json({
+            message: "User Created Successfully 👤✅",
+            user: newUser
+        })
+        
+        
+    } catch (error) {
+        console.log("Error occured - " + error.messsage);
+        res.status(500).json({
+            message: "API Error Occured! 🤖❤️"
+        })
+    }
+}
--- index.js
+++ index.js
@@ -2,9 +2,12 @@ import e from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import DB from "./config/db.js";
+import cors from "cors";
 
 const app = e();
+app.use(cors());
 dotenv.config();
+app.use(e.json());
 
 const port = process.env.PORT || 5000;
 
